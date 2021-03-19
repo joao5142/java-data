cafbb82d2741b2f89a56fb186ea5adefca0ecf36
joao5142 <joaopaulo3687@hotmail.com>
2021-03-19 17:38:00 -0300

    Projeto Criado

---
+++ src/calendar/Program.java
@@ -0,0 +1,31 @@
+package calendar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		 Scanner sc= new Scanner(System.in);
+		 
+         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+         Date data = new Date();
+         
+         Date data2= sdf.parse("15/02/2000 16:41:23");
+         
+         System.out.println(sdf.format(data2));
+         System.out.println(data);
+     
+         Calendar cal= Calendar.getInstance();
+         cal.setTime(data);
+         cal.add(Calendar.MONTH, 1);
+         data= cal.getTime();
+         
+         System.out.println(sdf.format(data));
+	}
+
+}
